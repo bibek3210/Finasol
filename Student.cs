e0436971e197b7bb6a142410e3a5a5b4c8b377b1
bibek3210 <37579985+bibek3210@users.noreply.github.com>
2018-05-08 14:37:09 +0545

    Add files via upload

---
+++ Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace crud.Models
+{
+    public class Student
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+        public string Secret { get; set; }
+
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
+ 
